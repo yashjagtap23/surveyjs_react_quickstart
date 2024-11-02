006b18b2ef6b03f87fee194711940f62d4de6bbf
yashjagtap23 <108370176+yashjagtap23@users.noreply.github.com>
2024-11-02 15:29:33 -0500

    Added Dark Mode

--- src/pages/Survey.js
+++ src/pages/Survey.js
@@ -1,11 +1,9 @@
-import React from "react";
-import { Model, StylesManager } from "survey-core";
+import React, { useState } from "react";
+import { Model } from "survey-core";
 import { Survey } from "survey-react-ui";
 import "survey-core/defaultV2.css";
-
-import { json } from "../data/survey_json.js";
-
-StylesManager.applyTheme("defaultV2");
+import { LayeredDark } from "survey-core/themes";
+import { json } from "../data/survey_json.js"; // Your survey JSON
 
 function onValueChanged(_, options) {
   console.log("New value: " + options.value);
@@ -16,10 +14,24 @@ function onComplete(survey) {
 }
 
 export function SurveyPage() {
+  const [isDarkMode, setIsDarkMode] = useState(true); // Assuming default dark mode
+
+  // Initialize the survey model and apply the theme
   const model = new Model(json);
+  if (isDarkMode) {
+    model.applyTheme(LayeredDark); // Apply the LayeredDarkPanelless theme
+  }
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    <div className="container">
+    <div className={`container ${isDarkMode ? "survey-dark-mode" : ""}`}>
       <h1>22BC Survey</h1>
+      <button onClick={toggleDarkMode}>
+        Toggle {isDarkMode ? "Light" : "Dark"} Mode
+      </button>
       <Survey
         model={model}
         onComplete={onComplete}

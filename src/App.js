8fbf09b699503524325f19ab61954501946b22e5
yashjagtap23 <108370176+yashjagtap23@users.noreply.github.com>
2024-11-02 15:13:40 -0500

    Deleted Unneccesary

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
 import { HomePage } from "./pages/Home";
-import { CreatorPage } from "./pages/Creator";
 import { SurveyPage } from "./pages/Survey";
-import { ExportToPDFPage } from "./pages/Export";
-import { AnalyticsPage } from "./pages/Analytics";
-import { AnalyticsTabulatorPage } from "./pages/AnalyticsTabulator";
-import { AnalyticsDatatablesPage } from "./pages/AnalyticsDatatables";
-
 import "bootstrap/dist/css/bootstrap.css";
 
 import * as Survey from "survey-core";
@@ -65,23 +59,6 @@ export default function SurveyJSReactApplication() {
               <li>
                 <Link to="/survey">Survey</Link>
               </li>
-              <li>
-                <Link to="/creator">Survey Creator</Link>
-              </li>
-              <li>
-                <Link to="/export">PDF Export</Link>
-              </li>
-              <li>
-                <Link to="/analytics">Analytics</Link>
-              </li>
-              <li>
-                <Link to="/analyticstabulator">Results Table</Link>
-              </li>
-              <li>
-                <Link to="/analyticsdatatables">
-                  Results Table (IE Support)
-                </Link>
-              </li>
             </ul>
           </div>
         </nav>
@@ -93,21 +70,6 @@ export default function SurveyJSReactApplication() {
             </Route>
             <Route path="/survey">
               <SurveyPage />
-            </Route>
-            <Route path="/creator">
-              <CreatorPage />
-            </Route>
-            <Route path="/export">
-              <ExportToPDFPage />
-            </Route>
-            <Route path="/analytics">
-              <AnalyticsPage />
-            </Route>
-            <Route path="/analyticsdatatables">
-              <AnalyticsDatatablesPage />
-            </Route>
-            <Route path="/analyticstabulator">
-              <AnalyticsTabulatorPage />
             </Route>
           </Switch>
         </div>
